6efad6d896baaff7e6d542c3ede8091ebdb2d4ac
ChrisDjMh <chrisdjmh@gmail.com>
2024-11-27 13:06:19 -0600

    Update InicioDeSesion.php

--- PerfilMedicos/Principal/InicioDeSesion.php
+++ PerfilMedicos/Principal/InicioDeSesion.php
@@ -18,7 +18,7 @@ include ('validar.php');
         <nav>
             <img src="logo.png" class="logo">
             <ul>
-                <li> <a href="../../Inicio/inicio.php">Home</a></li>
+                <li> <a href="inicio.php">Home</a></li>
             </ul>
         </nav>
     </div>
@@ -35,8 +35,8 @@ include ('validar.php');
             </div>
             <input type="submit" name="button" value="Iniciar">
             <div class="registrarse">
-                No tienes cuenta? Crea una! <a href="../../Inicio/nuevo.php">registro</a><br>
-                Eres Paciente? <a href="../../Login/iniciarsesionpaciente.php">Inicia Sesion aqui!</a>
+                No tienes cuenta? Crea una! <a href="nuevo.php">registro</a><br>
+                Eres Paciente? <a href="iniciarsesionpaciente.php">Inicia Sesion aqui!</a>
             </div>
         </form>
     </div>
