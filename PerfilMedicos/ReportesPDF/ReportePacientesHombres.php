8b372a9a5bd7aa48aa6baae2fbcb656612317082
ChrisDjMh <chrisdjmh@gmail.com>
2024-11-27 13:15:38 -0600

    Update ReportePacientesHombres.php

--- PerfilMedicos/ReportesPDF/ReportePacientesHombres.php
+++ PerfilMedicos/ReportesPDF/ReportePacientesHombres.php
@@ -1,7 +1,7 @@
 <?php
-require('../fpdf/fpdf.php');
-include('../Principal/funciones.php');
-include ('../Principal/conexion.php');
+require('fpdf/fpdf.php');
+include('funciones.php');
+include ('conexion.php');
 $con = connection();
 session_start();
 $IdMedico = $_SESSION['IdMedico'];
@@ -27,7 +27,7 @@ function Header()
     $this->Cell(60);
     $this->Cell(70,10,'Pacientes De Genero ' .  $_GET['Genero'],0,0,'C');
     $this->Ln(15);
-    $this->Image('../Principal/logo.png', 10, 0, 30, 30);
+    $this->Image('logo.png', 10, 0, 30, 30);
     $this->SetFont('Times','',8);
     $this->SetTextColor(0, 0, 0);
     $AcomodarTexto = 30;
